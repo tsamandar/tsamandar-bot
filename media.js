291644e8b9ecc3d27fb96c38c22583bbebdae319
tsamandar <65559245+tsamandar@users.noreply.github.com>
2021-04-08 22:33:51 +0500

    sw

--- media.js
+++ media.js
@@ -1,10 +1,9 @@
-const { Telegraf } = require('telegraf');
-const dotenv = require('dotenv');
+const { Telegraf } = require("telegraf");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('start', ctx => ctx.reply('Hi'));
+bot.command("start", (ctx) => ctx.reply("Hi"));
 
 bot.launch();
-
