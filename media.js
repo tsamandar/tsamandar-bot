f777e69e3e464e97c69a190d663dce1c45395a5b
tsamandar <65559245+tsamandar@users.noreply.github.com>
2021-04-08 22:39:05 +0500

    dsd

--- media.js
+++ media.js
@@ -4,6 +4,13 @@ dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command("start", (ctx) => ctx.reply("Hi"));
+bot.command("start", (ctx) => ctx.reply("Text sent through telegraf's reply method", {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: "Button 4", callback_data: "button4" }, { text: "Button 5", callback_data: "button5" }],
+        [{ text: "Button 6", callback_data: "button6" }]
+      ]
+    }
+  }))
 
 bot.launch();
