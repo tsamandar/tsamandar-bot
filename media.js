16fb439c6a71f56ab2e6a6999b4576fa64ee5d22
tsamandar <65559245+tsamandar@users.noreply.github.com>
2021-04-08 22:15:31 +0500

    ll

--- media.js
+++ media.js
@@ -1,40 +1,10 @@
 const { Telegraf } = require('telegraf');
-const fs = require('fs');
-const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('start', (ctx) => ctx.reply('Salom, ${ctx.from}'))
-
-// reading from media
-bot.command('logo_apple', (ctx) => ctx.replyWithPhoto({ source: 'media/apple.png' }))
-bot.command('logo_microsoft', (ctx) => ctx.replyWithPhoto({ source: 'media/microsoft.png' }))
-bot.command('logo_google', (ctx) => ctx.replyWithPhoto({ source: 'media/google.png' }))
-bot.command('logo_facebook', (ctx) => ctx.replyWithPhoto({ source: 'media/facebook.png' }))
-bot.command('logo_telegram', (ctx) => ctx.replyWithPhoto({ source: 'media/telegram.png' }))
-
-// reading media as streaming
-bot.command('intro_1', (ctx) => ctx.replyWithVideo({
-    source: fs.createReadStream('media/intro.mp4')
-}));
-
-bot.command('intro_2', (ctx) => ctx.replyWithVideo({
-    source: 'media/intro.mp4'
-}));
-
-// search media from the internet
-bot.command('random_photo', (ctx) => ctx.replyWithPhoto('https://picsum.photos/200/300/?random'));
-
-// reply photo with caption
-bot.command('meme', (ctx) => ctx.replyWithPhoto(
-    { source: 'media/meme.jpg' },
-    {
-        caption: 'No <br>Comment</>',
-        parse_mode: 'HTML'
-    }
-))
+bot.command('start', ctx => ctx.reply('Hi'));
 
 bot.launch();
 
